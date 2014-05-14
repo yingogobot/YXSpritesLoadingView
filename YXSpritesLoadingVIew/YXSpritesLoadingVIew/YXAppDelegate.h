90bd807355fbd5a93de7dbbbc53fd435b557859b
Yin Xu <yinxu@gogobot.com>
2014-05-14 12:14:15 -0700

    Create Project

---
+++ YXSpritesLoadingVIew/YXSpritesLoadingVIew/YXAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  YXAppDelegate.h
+//  YXSpritesLoadingVIew
+//
+//  Created by Yin Xu on 5/14/14.
+//  Copyright (c) 2014 YinXuApp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YXAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
